642686fa2fdcc3041213689732702d4c374df607
THE_cattail <THE_cattail@k-neco.moe>
2023-11-07 17:36:26 +0800

    feat(rust): add log and binary mod

---
+++ lib/rust/src/log.rs
@@ -0,0 +1,18 @@
+use color_eyre::eyre::Result;
+use tracing::Level;
+
+#[cfg(debug_assertions)]
+const DEFAULT_MAX_LEVEL: Level = Level::DEBUG;
+#[cfg(not(debug_assertions))]
+const DEFAULT_MAX_LEVEL: Level = Level::INFO;
+
+pub fn init(crate_name: &'static str) -> Result<()> {
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_env_filter(format!("{crate_name}={DEFAULT_MAX_LEVEL}"))
+        .init();
+
+    color_eyre::install()?;
+
+    Ok(())
+}
